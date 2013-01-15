e01519fe3e9eab7a52b212ab19e43f67650b7a59
Tom Skroza <tom.skroza@marketingmix.com.au>
2013-01-15 11:25:26 +0800

    Adverts open in new tab.

--- js/ad-ittjs.php
+++ js/ad-ittjs.php
@@ -3,16 +3,16 @@ header("Content-Type: text/javascript"); ?>
 jQuery(window).load(function() {
 
 	<?php if (get_option('top_ad_link') != "" && get_option('top_ad_img') != "") {
-			echo("jQuery(\"body\").prepend(\"<a id='top_ad' href='".get_option('top_ad_link')."'><img src='".get_option('top_ad_img')."' /></a>\");");
+			echo("jQuery(\"".get_option('css_content_wrapper_selector')."\").prepend(\"<a target='_blank' id='top_ad' href='".get_option('top_ad_link')."'><img src='".get_option('top_ad_img')."' /></a>\");");
 	} ?>
 	<?php if (get_option('bottom_ad_link') != "" && get_option('bottom_ad_img') != "") {
-		echo("jQuery(\"body\").append(\"<a id='bottom_ad' href='".get_option('bottom_ad_link')."'><img src='".get_option('bottom_ad_img')."' /></a>\");");
+		echo("jQuery(\"".get_option('css_content_wrapper_selector')."\").append(\"<a target='_blank' id='bottom_ad' href='".get_option('bottom_ad_link')."'><img src='".get_option('bottom_ad_img')."' /></a>\");");
 	} ?>
 	<?php if (get_option('left_ad_link') != "" && get_option('bottom_ad_img') != "") {
-		echo("jQuery(\"body\").append(\"<a id='left_ad' href='".get_option('left_ad_link')."'><img src='".get_option('left_ad_img')."' /></a>\");");
+		echo("jQuery(\"body\").append(\"<a target='_blank' id='left_ad' href='".get_option('left_ad_link')."'><img src='".get_option('left_ad_img')."' /></a>\");");
 	} ?>
 	<?php if (get_option('right_ad_link') != "" && get_option('right_ad_img') != "") {
-		echo("jQuery(\"body\").append(\"<a id='right_ad' href='".get_option('right_ad_link')."'><img src='".get_option('right_ad_img')."' /></a>\");");
+		echo("jQuery(\"body\").append(\"<a target='_blank' id='right_ad' href='".get_option('right_ad_link')."'><img src='".get_option('right_ad_img')."' /></a>\");");
 	} ?>
 	var position = jQuery("<?php echo(get_option('css_content_wrapper_selector')); ?>").offset();
   var left = parseInt(position.left - parseInt(jQuery("#left_ad").css("width")));
